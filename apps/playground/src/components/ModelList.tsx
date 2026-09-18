8ed19f69f8b6e80c1e94ce49911968e997197ad2
myrQyry <99147786+myrqyry@users.noreply.github.com>
2026-09-17 22:53:11 -0500

    style: use speech bubble for language models

--- apps/playground/src/components/ModelList.tsx
+++ apps/playground/src/components/ModelList.tsx
@@ -107,7 +107,8 @@ function FamilyGlyph({ family, className = '' }: { family: ModelFamily; classNam
   if (family === 'Language') {
     return (
       <svg {...common}>
-        <path d="M5 6h14M8 6v12m8-12v12M5 18h14" />
+        <path d="M5 5.5h14a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2h-7l-4.5 3v-3H5a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2Z" />
+        <path d="M7 9h10M7 12.5h7" />
       </svg>
     )
   }
